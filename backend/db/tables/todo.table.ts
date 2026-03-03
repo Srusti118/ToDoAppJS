a60ca3b98a876111b157307ee0728d1204dd170d
Srusti <patelsrusti455@gmail.com>
2026-03-03 17:15:29 +0530

    deployment error solved v4

--- backend/db/tables/todo.table.ts
+++ backend/db/tables/todo.table.ts
@@ -2,11 +2,11 @@ import { BaseTable } from '../baseTable.js';
 
 export class TodoTable extends BaseTable {
     readonly table = 'todos';
-    columns = (this as any).setColumns((t: any) => ({
+    columns = this.setColumns((t) => ({
         id: t.identity().primaryKey(),
         userId: t.integer().foreignKey('users', 'id').index().nullable(),
         text: t.text(),
         done: t.boolean().default(false),
-        timestamp: t.timestamps(),
+        ...t.timestamps(),
     }));
 }
